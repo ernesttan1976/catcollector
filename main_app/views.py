412106257be83dd64c2704600b34e7896f58f27b
ernesttan1976 <ernesttan1976@gmail.com>
2023-07-03 16:14:22 +0800

    about and cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+
+cats = [
+    {
+        "name": "Lolo",
+        "breed": "tabby",
+        "description": "furry little demon",
+        "age": 3,
+    },
+        {
+        "name": "Sachi",
+        "breed": "calico",
+        "description": "gentle and loving",
+        "age": 2,
+    },
+]
+
 # Create your views here.
+
+#home view
+def home(request):
+    return render(request, 'home.html')
+
+#about view
+def about(request):
+    return render(request, 'about.html')
+
+#cats view
+def cats_index(request):
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
